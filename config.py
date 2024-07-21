db62fc43871b133c033e5f31a56063c8ca183bf7
Jean-Léon HENRY <92361407+JeanLeonHenry@users.noreply.github.com>
2024-07-21 14:20:33 +0200

    config update: no need to hide the api urls

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ from dotenv import dotenv_values
 poster_filename = "poster.png"
 info_filename = "media_info.json"
 config_path = ""
+API_URL = "https://api.themoviedb.org/3/"
+IMAGE_API_URL = "http://image.tmdb.org/t/p/w500"
+
 config = dotenv_values(config_path)
 API_KEY = config["API_KEY"]
 API_READ_TOKEN = config["API_READ_TOKEN"]
 DB_PATH = config["DB_PATH"]
-API_URL = config["API_URL"]
-IMAGE_API_URL = config["IMAGE_API_URL"]
