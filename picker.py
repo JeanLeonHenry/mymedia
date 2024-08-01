ee0c03b5aaea56424e4abf55cf70775d8c0ff079
Jean-Léon HENRY <92361407+JeanLeonHenry@users.noreply.github.com>
2024-08-01 11:10:15 +0200

    removed redundant sorting

--- picker.py
+++ picker.py
@@ -3,7 +3,7 @@ from operator import itemgetter
 
 dbh = DataBaseHandler()
 data = dbh.get()
-for media in sorted(data, key=itemgetter(2)):
+for media in data:
     id, media_type, title, year, overview, director, path = media
     print(
         f"{title}\t{title} ({year}){' -- '+director if director else ''}\t{overview}\t{path}"
